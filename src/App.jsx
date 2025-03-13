3cb320d161a99db17e95519b02d43f38921a6733
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2025-03-13 11:05:39 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/reusable/Button'
+import Navbar from './components/layouts/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    
+    </>
+  )
+}
+
+export default App
